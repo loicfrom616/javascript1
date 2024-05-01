faf572f5f031c3f2c11140f5f624703c8efbb067
loicfrom616 <mawash@mailo.com>
2024-05-01 18:42:14 -0400

    Authorization

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -39,10 +39,29 @@ switch (userType) {
         userCategory = "Unknown";
 }
 
-console.log("User Message:", userMessage);
-console.log("Access Level:", accessLevel);
-console.log("User Category:", userCategory);
-console.log("Authentication Status:", authenticationStatus);
+// console.log("User Message:", userMessage);
+// console.log("Access Level:", accessLevel);
+// console.log("User Category:", userCategory);
+// console.log("Authentication Status:", authenticationStatus);
 
 let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+let profile = "Employee";
+let authorization;
+
+switch (profile) {
+    case "Employee":
+        authorization = "Dietary Services";
+        break;
+    case "Enrolled Member" :
+        authorization = "Dietary Services and one on one interaction with a dietician";
+        break;
+    case "Subscriber" :
+        authorization = "Limited Dietary Services";
+        break ;
+    default :
+    authorization = "Please enroll or subscribe";
+}
+
+console.log(authorization);

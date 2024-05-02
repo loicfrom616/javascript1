7baa674f900923bb0b436a2b1f2ae243e38dd79d
loicfrom616 <mawash@mailo.com>
2024-05-02 08:02:20 -0400

    Calculate grocery

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,13 @@ function calculateArea() {
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
 
+
+function groceryTracker () {
+    price1 = parseFloat(document.getElementById('grocery1').value);
+    price2 = parseFloat(document.getElementById('grocery2').value);
+    price3 = parseFloat(document.getElementById('grocery3').value);
+
+    let sum = price1 + price2 + price3 ;
+
+    document.getElementById('total').innerText = `Price total is : ${sum}`;
+}
